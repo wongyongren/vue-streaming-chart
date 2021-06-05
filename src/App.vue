1069a4b21ce7710c9ceaa24b8bf3cf2d8f5ee136
wongyongren <wongyongren@hotmail.com>
2021-06-05 12:57:54 +0800

    123

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <MyChart />
+  </div>
+</template>
+
+
+<script>
+import MyChart from "./components/MyChart.vue";
+
+export default {
+  name: "app",
+
+  components: {
+    MyChart,
+  },
+};
+</script>
